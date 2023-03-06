20e50d61bd6d0b70f26862a9f651da17b8af90b9
Jeanette Sperhac <jsperhac@ucsd.edu>
2023-03-06 11:03:13 -0500

    Specify PHP CLI for cron job

--- conf/hubzero-metrics.cron.d
+++ conf/hubzero-metrics.cron.d
@@ -1,7 +1,11 @@
-*/15 * * * *	www-data	/usr/share/hubzero-metrics/scripts/xlogfix_whoisonline
-10 0 * * *	www-data	/usr/share/hubzero-metrics/scripts/import/__fetch_apache_and_auth_log.sh
-15 0 * * *	www-data	/usr/share/hubzero-metrics/scripts/import/__import_apache_and_auth_log.sh
-30 0 * * *	www-data	/usr/share/hubzero-metrics/scripts/import/__archive_apache_and_auth_log.sh
-40 0 * * *	www-data	/usr/share/hubzero-metrics/scripts/__process_tool_metrics.sh
-50 0 * * *	www-data	/usr/share/hubzero-metrics/scripts/__process_usage_metrics.sh
-50 1 1 * *	www-data	/usr/share/hubzero-metrics/scripts/__process_usage_metrics_summary.sh
+# Metrics cron jobs
+# Format is:
+# min hour day-of-month month day-of-week user command...
+#
+*/5 * * * * apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/scripts/xlogfix_whoisonline > /var/log/metrics/xlogfix.log
+10 0 * * *  apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/scripts/import/__fetch_apache_and_auth_log.sh
+15 0 * * *  apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/scripts/import/__import_apache_and_auth_log.sh
+30 0 * * *  apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/scripts/import/__archive_apache_and_auth_log.sh
+40 0 * * *  apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/scripts/__process_tool_metrics.sh
+50 0 * * *  apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/scripts/__process_usage_metrics.sh
+50 1 1 * *  apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/scripts/__process_usage_metrics_summary.sh
