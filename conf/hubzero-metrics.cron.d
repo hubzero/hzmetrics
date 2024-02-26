d4923f512ab8f6579cd42a1c6b9e9f3050805253
Jeanette Sperhac <jsperhac@ucsd.edu>
2024-02-26 16:04:59 -0500

    Remove outdated export command from cron job script

--- conf/hubzero-metrics.cron.d
+++ conf/hubzero-metrics.cron.d
@@ -2,10 +2,10 @@
 # Format is:
 # min hour day-of-month month day-of-week user command...
 #
-*/5 * * * * apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/xlogfix_whoisonline.php > /var/log/metrics/xlogfix.log
-10 0 * * *  apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/import/__fetch_apache_and_auth_log.sh
-15 0 * * *  apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/import/__import_apache_and_auth_log.sh
-30 0 * * *  apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/import/__archive_apache_and_auth_log.sh
-40 0 * * *  apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/__process_tool_metrics.sh
-50 0 * * *  apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/__process_usage_metrics.sh
-50 1 1 * *  apache  export PHPRC=/opt/remi/php56/root/etc/php-cli.ini;  /opt/hubzero/bin/metrics/__process_usage_metrics_summary.sh
+*/5 * * * * apache  /opt/hubzero/bin/metrics/xlogfix_whoisonline.php > /var/log/metrics/xlogfix.log
+10 0 * * *  apache  /opt/hubzero/bin/metrics/import/__fetch_apache_and_auth_log.sh
+15 0 * * *  apache  /opt/hubzero/bin/metrics/import/__import_apache_and_auth_log.sh
+30 0 * * *  apache  /opt/hubzero/bin/metrics/import/__archive_apache_and_auth_log.sh
+40 0 * * *  apache  /opt/hubzero/bin/metrics/__process_tool_metrics.sh
+50 0 * * *  apache  /opt/hubzero/bin/metrics/__process_usage_metrics.sh
+50 1 1 * *  apache  /opt/hubzero/bin/metrics/__process_usage_metrics_summary.sh
